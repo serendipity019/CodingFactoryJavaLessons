6578aec96fdcd89e0b1f21b62b9b36e3df65f721
serendipity019 <navagio09@gmail.com>
2024-10-28 11:59:28 +0200

    exercises week 1

---
+++ src/gr/aueb/cf/ch1/SumApp.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.ch1;
+
+public class SumApp {
+    public static void main(String[] args) {
+        int num1 = 19;
+        int num2 = 30;
+        int sum = 0;
+
+        sum = num1 +num2;
+        System.out.println("Το αποτέλεσμα της πρόσθεσης είναι ίσο με " + sum);
+
+    }
+}
