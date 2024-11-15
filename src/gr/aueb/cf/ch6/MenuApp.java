be2462c4c8344d76ba8c8fa8b12e47fcf2e6d2b2
serendipity019 <navagio09@gmail.com>
2024-11-15 17:59:07 +0200

    update MenuApp.java

--- src/gr/aueb/cf/ch6/MenuApp.java
+++ src/gr/aueb/cf/ch6/MenuApp.java
@@ -1,5 +1,11 @@
 package gr.aueb.cf.ch6;
 
+/**
+ * Εμφανίζει ένα μενου επιλογών και ο χρήστης
+ * επιλέγει ένα menu item και στην συνέχεια ανάλογα με την επιλογή
+ * εκτελείτε κάποια ενέργεια.
+ */
+
 import java.util.Scanner;
 
 public class MenuApp {
@@ -10,6 +16,12 @@ public class MenuApp {
         do{
             printMenu();
             choice = sc.nextInt();
+            if (!isChoiceValid(choice)){
+                System.out.println("Error. Choice not valid.");
+                continue;
+            }
+
+            doOnChoice(choice);
         }while (choice != 5);
     }
 
@@ -21,4 +33,33 @@ public class MenuApp {
         System.out.println("4. Ενημέρωση");
         System.out.println("5. Εξοδος");
     }
+
+    public static boolean isChoiceValid(int choice){
+        return choice >= 1 && choice <= 5;
+    }
+
+    public static void doOnChoice( int choice){
+        switch (choice){
+            case 1:
+                System.out.println("Εισαγωγή process");
+                break;
+            case 2:
+                System.out.println("Διαγραφή process");
+                break;
+            case 3:
+                System.out.println("Αναζήτηση process");
+                break;
+            case 4:
+                System.out.println("Ενημέρωση process");
+                break;
+            case 5:
+                System.out.println("Εξοδος");
+                break;
+            default:
+                System.out.println("Λάθος επιλογή");
+                break;
+        }
+    }
+
+
 }
