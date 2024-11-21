8c06d39920092a39f56ece8bde91915093bac169
serendipity019 <navagio09@gmail.com>
2024-11-21 17:14:12 +0200

    example in arrays in lesson 9

---
+++ src/gr/aueb/cf/ch8/SymmetricApp.java
@@ -0,0 +1,33 @@
+package gr.aueb.cf.ch8;
+
+public class SymmetricApp {
+    public static void main(String[] args) {
+
+    }
+
+    public  static  boolean isSymmetric(int[] arr){
+        boolean isSymmetric = true;
+      for( int i = 0, j= arr.length - 1; i < j ;i++, j--){
+          if (arr[i] != arr[j]){
+              isSymmetric = false;
+              break;
+          }
+      }
+        return isSymmetric;
+    }
+
+    // second manner
+    public  static  boolean isArrSymmetric( int[] arr){
+        boolean isSymmetric = true;
+        int n = arr.length - 1;
+
+        for (int i = 0; i < n / 2; i++){
+            if (arr[i] != arr[n - 1]){
+                isSymmetric = false;
+                break;
+            }
+
+        }
+        return isSymmetric;
+    }
+}
