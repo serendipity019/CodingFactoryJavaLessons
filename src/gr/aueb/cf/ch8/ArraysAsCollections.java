63ec103bb2b8a3e6d5e02d2ef9a7d478304e5b7b
serendipity019 <navagio09@gmail.com>
2024-11-25 20:22:18 +0200

    update because i have error

--- src/gr/aueb/cf/ch8/ArraysAsCollections.java
+++ src/gr/aueb/cf/ch8/ArraysAsCollections.java
@@ -83,7 +83,7 @@ public class ArraysAsCollections {
 
     public static boolean moreThanTwoConsecutive(int[] arr){
         int count = 0;
-        for (int i = 0; i < arr.lenght -2; i++){
+        for (int i = 0; i < arr.length -2; i++){
             if( arr[i] == arr[i + 1] - 1 && arr[i] == arr[i+2] - 2){
                 count++;
                 break;
