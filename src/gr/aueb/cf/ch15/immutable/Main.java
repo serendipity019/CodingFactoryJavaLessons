aa913e29e0d282c7182bc202a4afa554226f174a
serendipity019 <navagio09@gmail.com>
2025-01-16 00:27:10 +0200

    examples from immutable classes

---
+++ src/gr/aueb/cf/ch15/immutable/Main.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.ch15.immutable;
+
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(5,6);
+        Point3D point3D = new Point3D(point, 8);
+    }
+}
