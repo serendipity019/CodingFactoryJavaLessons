4eb719cc753c3cc9cd2310772d43ed9a030eb5d7
serendipity019 <navagio09@gmail.com>
2025-01-21 12:21:14 +0200

    an other example with skeletal programming

---
+++ src/gr/aueb/cf/ch17/skeletal2/IRectangle.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.ch17.skeletal2;
+
+public interface IRectangle {
+    double getArea();
+    double getPerimeter();
+    boolean isSquare();
+}
---
+++ src/gr/aueb/cf/ch17/skeletal2/ISquare.java
@@ -0,0 +1,5 @@
+package gr.aueb.cf.ch17.skeletal2;
+
+public interface ISquare {
+    double getDiagonal();
+}
