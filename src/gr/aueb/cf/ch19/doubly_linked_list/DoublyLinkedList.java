0ec72ff84ba81d6f1f1c5f6fc283d3d5f685bd4c
serendipity019 <navagio09@gmail.com>
2025-02-05 10:05:27 +0200

    feat: add traverseReverse, traverse, removeFirst, removeLast in DoublyLinkedList

--- src/gr/aueb/cf/ch19/doubly_linked_list/DoublyLinkedList.java
+++ src/gr/aueb/cf/ch19/doubly_linked_list/DoublyLinkedList.java
@@ -40,5 +40,70 @@ public class DoublyLinkedList<T> {
         tail = tmp;
     }
 
+    /**
+     * Delete the first node of the list. The time complexity is O(1).
+     * @return the node where deleted.
+     */
+    public Node<T> removeFirst() {
+        Node<T> nodeToReturn;
+
+        if (isEmpty()) return null;
+
+        nodeToReturn = head;
+        if (head.getNext() == null) tail = null;
+        head = head.getNext();
+        head.setPrev(null);
+        return nodeToReturn;
+    }
+
+    /**
+     * Delete the last node of the list. Time complexity O(1).
+     * @return the node where deleted.
+     */
+    public Node<T> removeLast() {
+        Node<T> nodeToReturn;
+        if (isEmpty() || head.getNext() == null) return removeFirst();
+
+        nodeToReturn = tail;
+        tail = tail.getPrev();
+        tail.setNext(null);
+        return nodeToReturn;
+    }
+
+    public Node<T> get(T t) {
+        Node<T> nodeToReturn = null;
+
+        for (Node<T> n = head; n != null; n = n.getNext()) {
+            if (n.getItem().equals(t)) {
+                nodeToReturn = n;
+                break;
+            }
+        }
+
+        return nodeToReturn;
+    }
+
+    /**
+     * Time complexity O(n). Print the items in the list.
+     */
+    public void traverse() {
+        for (Node<T> n = head; n != null; n = n.getNext()) {
+            System.out.println(n.getItem());
+        }
+    }
+
+    /**
+     * Time complexity O(n). Print the items in the list(back to front).
+     */
+    public void traverseReverse() {
+        for (Node<T> n = tail; n != null; n = n.getPrev()) {
+            System.out.println(n.getItem());
+        }
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
 
 }
