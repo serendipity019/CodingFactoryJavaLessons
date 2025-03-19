feb393ee9e4294989580054d12247cfdfeae322d
serendipity019 <navagio09@gmail.com>
2025-03-19 18:52:26 +0200

    feat: We create DateTimeServer

---
+++ src/gr/aueb/cf/ch26/DateTimeServer.java
@@ -0,0 +1,35 @@
+package gr.aueb.cf.ch26;
+
+import gr.aueb.cf.ch2.Date;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class DateTimeServer extends Thread {
+
+    @Override
+    public void run() {
+        ServerSocket servFd; //File Descriptor
+        int serverPort = 13;
+
+        try {
+            servFd = new ServerSocket();
+            servFd.bind(new InetSocketAddress("127.0.0.1", serverPort), 100); // The number 100 mean the que of the package where waiting. The default is 50.
+
+            while (true) {
+                Socket connectedFd = servFd.accept();
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connectedFd.getOutputStream()));
+                bw.write(new Date().toString());
+                bw.flush(); // This is make it to not wait to fill first the buffer for write.
+                connectedFd.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
