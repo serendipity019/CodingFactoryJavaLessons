cd40993454e69dd6ca6242a1879f21f5c26a1a85
serendipity019 <navagio09@gmail.com>
2025-03-19 18:35:20 +0200

    feat: we create DateTimeClient using Socket and StringBuilder with BufferedReader

---
+++ src/gr/aueb/cf/ch26/DateTimeClient.java
@@ -0,0 +1,39 @@
+package gr.aueb.cf.ch26;
+
+import javax.security.sasl.SaslClient;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class DateTimeClient {
+    public static void main(String[] args) {
+        Socket socket = null;
+        StringBuilder sb = new StringBuilder();
+        BufferedReader bf = null;
+
+        try {
+            InetAddress servAddress = InetAddress.getByName("time.nist.gov");
+            int servPort = 13;
+            socket = new Socket(servAddress,servPort);
+            bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String line = "";
+
+            while ((line = bf.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+
+            System.out.println("Server DateTime: " + sb);
+        } catch (IOException e) {
+
+        } finally {
+            try {
+                if (socket != null) socket.close();
+                if (bf != null) bf.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
